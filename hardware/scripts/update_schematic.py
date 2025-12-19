bdca7bddad4bab84d11a3f636951ef730e62088c
DumpsterDoofus <dumpsterdoofus@gmail.com>
2025-12-18 21:02:44 -0500

    Ok

--- hardware/scripts/update_schematic.py
+++ hardware/scripts/update_schematic.py
@@ -1,15 +1,11 @@
-with open('/home/peter/Documents/git/keyboard_accordion/hardware/kicad/96_key/96_key.kicad_sch') as schematic_file:
+with open('/home/peter/Documents/git/keyboard_accordion/hardware/kicad/daughterboard/daughterboard.kicad_sch') as schematic_file:
     schematic = schematic_file.read()
 
-s = 'AM'
-i = 5
-j = 15
+s = 'CONTROL'
+i = 3
 while i >= 0:
-    while j >= 0:
-        schematic = schematic.replace(f'"{s}{i}:{j}"', f'"{s}{i+1}:{j+1}"')
-        j -= 1
+    schematic = schematic.replace(f'"{s}{i}"', f'"{s}{i+1}"')
     i -= 1
-    j = 15
 
-with open('/home/peter/Documents/git/keyboard_accordion/hardware/kicad/96_key/96_key.kicad_sch', 'w') as schematic_file:
+with open('/home/peter/Documents/git/keyboard_accordion/hardware/kicad/daughterboard/daughterboard.kicad_sch', 'w') as schematic_file:
     schematic_file.write(schematic)
